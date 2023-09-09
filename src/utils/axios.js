c4c312f6300ee0f1b5ecc7c6dcc048a1b12fb323
corookie <18945118328@163.com>
2023-09-09 22:25:09 +0800

    修改生产环境下请求协议

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,19 +1,11 @@
-/**
- * 严肃声明：
- * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
- * 本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
- * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
- * Copyright (c) 2020 陈尼克 all rights reserved.
- * 版权所有，侵权必究！
- */
  import axios from 'axios'
- import {showFailToast } from 'vant'
+ import { showToast, showFailToast } from 'vant'
  import { setLocal } from '@/common/js/utils'
  import router from '../router'
 
  console.log('import.meta.env', import.meta.env)
  
- axios.defaults.baseURL = import.meta.env.MODE == 'development' ? '//backend-api-01.newbee.ltd/api/v1' : '//backend-api-01.newbee.ltd/api/v1'
+ axios.defaults.baseURL = import.meta.env.MODE == 'development' ? '//backend-api-01.newbee.ltd/api/v1' : 'http://backend-api-01.newbee.ltd/api/v1'
  axios.defaults.withCredentials = true
  axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
  axios.defaults.headers['token'] = localStorage.getItem('token') || ''
@@ -40,4 +32,3 @@
  })
  
  export default axios
- 
