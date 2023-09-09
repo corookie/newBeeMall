b6e7018e8a09d0af721022bf315fa82685487a2a
corookie <18945118328@163.com>
2023-09-09 22:49:26 +0800

    大改

--- docs/assets/good-e06a3868.js
+++
@@ -1,1 +0,0 @@
-import{e as t}from"./index-ec7f1cc8.js";function a(e){return t.get(`/goods/detail/${e}`)}function o(){return t.get("/categories")}function s(e){return t.get("/search",{params:e})}export{a,o as g,s};
---
+++ docs/assets/good-f158d880.js
@@ -0,0 +1,1 @@
+import{e as t}from"./index-ff4fd389.js";function a(e){return t.get(`/goods/detail/${e}`)}function o(){return t.get("/categories")}function s(e){return t.get("/search",{params:e})}export{a,o as g,s};
